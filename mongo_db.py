c4620bd15c64920385be8e3ce8575cf7a8d23e71
khanhcfcf00 <124851411+khanhcfcf00@users.noreply.github.com>
2025-11-02 02:50:17 +0700

    Refactor MongoDB connection setup

--- mongo_db.py
+++ mongo_db.py
@@ -1,10 +1,11 @@
 from pymongo import MongoClient
 
-MONGO_URI = "mongodb+srv://khanhcfcf00:MRmagical123@cluster0.o5huuaw.mongodb.net/?retryWrites=true&w=majority"
-client = pymongo.MongoClient(MONGO_URI, tls=True, tlsCAFile=certifi.where())
+client = MongoClient("mongodb+srv://khanhcfcf00:MRmagical123@cluster0.o5huuaw.mongodb.net/")
+
 db = client["bill_scanner"]
 
 users_collection = db["users"]
 
 bills_collection = db["bills"]
+
 
