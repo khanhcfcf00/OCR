34b3a577dc846166c5f00dd5fcc118b56b87a600
khanhcfcf00 <124851411+khanhcfcf00@users.noreply.github.com>
2025-11-02 02:44:09 +0700

    Update mongo_db.py

--- mongo_db.py
+++ mongo_db.py
@@ -1,8 +1,10 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb+srv://khanhcfcf00:MRmagical123@cluster0.o5huuaw.mongodb.net/")
+MONGO_URI = "mongodb+srv://khanhcfcf00:MRmagical123@cluster0.o5huuaw.mongodb.net/?retryWrites=true&w=majority"
+client = pymongo.MongoClient(MONGO_URI, tls=True, tlsCAFile=certifi.where())
 db = client["bill_scanner"]
 
 users_collection = db["users"]
 
 bills_collection = db["bills"]
+
