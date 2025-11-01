c47b9e2cfa3cfbf731f840cdfdab9996395277f2
khanhcfcf00 <124851411+khanhcfcf00@users.noreply.github.com>
2025-11-02 02:34:56 +0700

    Update MongoDB connection to use SRV URI

--- mongo_db.py
+++ mongo_db.py
@@ -1,7 +1,8 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017")
+client = MongoClient("mongodb+srv://khanhcfcf00:MRmagical123@cluster0.o5huuaw.mongodb.net/")
 db = client["bill_scanner"]
 
 users_collection = db["users"]
+
 bills_collection = db["bills"]
